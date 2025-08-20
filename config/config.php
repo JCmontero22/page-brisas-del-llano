e2ab128094ed7fa256eae2463d98338a2b16582a
Johan Montero <johan00712@gmail.com>
2025-08-19 21:22:09 -0500

    Credenciales base de datos

--- config/config.php
+++ config/config.php
@@ -1,8 +1,13 @@
 <?php 
 
-    DEFINE('DB_HOST', 'localhost');
+    /* DEFINE('DB_HOST', 'localhost');
     DEFINE('DB_USER', 'root');
     DEFINE('DB_PASS', '');
-    DEFINE('DB_NAME', 'brisas_del_llano');
+    DEFINE('DB_NAME', 'brisas_del_llano'); */
 
     
+    define('DB_HOST', '127.0.0.1');
+    define('DB_NAME', 'jcmonter_dev');
+    define('DB_USER', 'jcmonter_dev94');
+    define('DB_PASS', 'Stratovarius94@');
+
