e7ba3ebf1dd757eb06c4408331ad25c6d88c72c7
Johan Montero <johan00712@gmail.com>
2025-10-17 22:09:59 -0500

    Actualizacion de base de datos e instagram

--- config/config.php
+++ config/config.php
@@ -5,11 +5,11 @@
     DEFINE('DB_PASS', '');
     DEFINE('DB_NAME', 'brisas_del_llano'); */
 
-    
+    /* PRODUCCION */
     define('DB_HOST', '127.0.0.1');
-    define('DB_NAME', 'jcmonter_dev');
-    define('DB_USER', 'jcmonter_dev94');
-    define('DB_PASS', 'Stratovarius94@');
+    define('DB_NAME', 'johanmo1_brisas_del_llano');
+    define('DB_USER', 'johanmo1_jcmontero_dev');
+    define('DB_PASS', 'camilomontero94@');
 
 
     
